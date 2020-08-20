a8851e1ec6e642112e4f1d50480d69fb1cf94683
Arjunb2811 <66243732+Arjunb2811@users.noreply.github.com>
2020-08-20 14:17:53 +0530

    Add files via upload

--- paper.js
+++ paper.js
@@ -1,5 +1,32 @@
 class Paper {
+constructor(x,y){
 
+var options = {
+friction: 0.7,
+density: 1,
+restitution: 0.8
+
+
+}
+this.body = Bodies.circle(x,y,15);
+this.radius = 15;
+World.add(world,this.body)
+
+
+}
+display(){
+var pos = this.body.position;
+var angle  =  this.body.angle;
+push();
+translate(pos.x,pos.y)
+rotate (angle)
+ellipseMode (RADIUS)
+fill ("blue")
+ellipse(0,0,this.radius,this.radius)
+pop();
+
+
+}
 
 
     
--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
+var paper1,box1,box2
 function preload()
 {
 	
@@ -17,8 +17,9 @@ function setup() {
 	world = engine.world;
 
 	//Create the Bodies Here.
-
-
+paper1 = new Paper(200,200)
+box1 = new Box(300,650,20,100)
+box2 = new Box(400,650,20,100)
 	Engine.run(engine);
   
 }
@@ -27,7 +28,9 @@ function setup() {
 function draw() {
   rectMode(CENTER);
   background(0);
-  
+  paper1.display();
+  box1.display();
+  box2.display();
   drawSprites();
  
 }
